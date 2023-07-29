dcddbe6cd550d8b0b116ad6fece49974399c23bc
Yanis Fourel <yanis.fourel@ledr.io>
2023-07-29 18:39:52 +0700

    Generic version

--- idxmap.go
+++ idxmap.go
@@ -1,44 +1,52 @@
 package idxmap
 
-type item struct {
-	key int
-	val int
+type item[K, V any] struct {
+	key K
+	val V
 }
 
-type IdxMap struct {
-	s []item
-	m map[int]int
+type IdxMap[K comparable, V any] struct {
+	s []item[K, V]
+	m map[K]int
 }
 
-func New() *IdxMap {
-	return &IdxMap{
-		s: make([]item, 0),
-		m: make(map[int]int),
+func New[K comparable, V any]() *IdxMap[K, V] {
+	return &IdxMap[K, V]{
+		s: make([]item[K, V], 0),
+		m: make(map[K]int),
 	}
 }
 
-func (im *IdxMap) Append(val int) {
-	im.s = append(im.s, item{0, val})
+func (im *IdxMap[K, V]) Append(val V) {
+	var k K
+	im.s = append(im.s, item[K, V]{
+		key: k,
+		val: val,
+	})
 }
 
-func (im *IdxMap) InsertKey(key int, val int) {
-	im.s = append(im.s, item{key, val})
+func (im *IdxMap[K, V]) InsertKey(key K, val V) {
+	im.s = append(im.s, item[K, V]{key, val})
 	im.m[key] = len(im.s) - 1
 }
 
-func (im *IdxMap) GetKey(key int) int {
+func (im *IdxMap[K, V]) GetKey(key K) (res V) {
 	idx, ok := im.m[key]
 	if !ok {
-		return 0
+		return
 	}
 	return im.s[idx].val
 }
 
-func (im *IdxMap) GetIdx(idx int) int {
+func (im *IdxMap[K, V]) GetIdx(idx int) V {
 	return im.s[idx].val
 }
 
-func (im *IdxMap) RemoveIdx(idx int) {
+func (im *IdxMap[K, V]) GetIdxKey(idx int) K {
+	return im.s[idx].key
+}
+
+func (im *IdxMap[K, V]) RemoveIdx(idx int) {
 	delete(im.m, im.s[idx].key)
 	im.s = append(im.s[:idx], im.s[idx+1:]...)
 	for k, i := range im.m {
@@ -48,7 +56,7 @@ func (im *IdxMap) RemoveIdx(idx int) {
 	}
 }
 
-func (im *IdxMap) RemoveKey(key int) {
+func (im *IdxMap[K, V]) RemoveKey(key K) {
 	idx, ok := im.m[key]
 	if !ok {
 		return
@@ -56,10 +64,10 @@ func (im *IdxMap) RemoveKey(key int) {
 	im.RemoveIdx(idx)
 }
 
-func (im *IdxMap) Iterate() []item {
+func (im *IdxMap[K, V]) Iterate() []item[K, V] {
 	return im.s
 }
 
-func (im *IdxMap) Len() int {
+func (im *IdxMap[K, V]) Len() int {
 	return len(im.s)
 }
--- idxmap_test.go
+++ idxmap_test.go
@@ -5,8 +5,8 @@ import (
 	"testing"
 )
 
-func TestSomething(t *testing.T) {
-	im := New()
+func TestEverything(t *testing.T) {
+	im := New[int, int]()
 
 	im.Append(1)
 	im.Append(2)
@@ -101,5 +101,43 @@ func TestSomething(t *testing.T) {
 	fmt.Println(im)
 	if im.Len() != 0 {
 		t.Errorf("Expected len 0, got %d", im.Len())
+	}
+
+	im2 := New[string, float64]()
+	im2.InsertKey("pi", 3.14159)
+	im2.InsertKey("e", 2.71828)
+	im2.Append(42)
+
+	fmt.Println(im2)
+
+	if im2.Len() != 3 {
+		t.Errorf("Expected len 3, got %d", im2.Len())
+	}
+	if im2.GetIdx(0) != 3.14159 {
+		t.Errorf("Index 0 should be 3.14159, got %f", im2.GetIdx(0))
+	}
+	if im2.GetIdx(1) != 2.71828 {
+		t.Errorf("Index 1 should be 2.71828, got %f", im2.GetIdx(1))
+	}
+	if im2.GetIdx(2) != 42 {
+		t.Errorf("Index 2 should be 42, got %f", im2.GetIdx(2))
+	}
+	if im2.GetKey("pi") != 3.14159 {
+		t.Errorf("Key pi should be 3.14159, got %f", im2.GetKey("pi"))
+	}
+	if im2.GetKey("e") != 2.71828 {
+		t.Errorf("Key e should be 2.71828, got %f", im2.GetKey("e"))
+	}
+	if im2.GetKey("42") != 0 {
+		t.Errorf("Key 42 should be 0, got %f", im2.GetKey("42"))
+	}
+	if im2.GetIdxKey(0) != "pi" {
+		t.Errorf("Index 0 should be pi, got %s", im2.GetIdxKey(0))
+	}
+	if im2.GetIdxKey(1) != "e" {
+		t.Errorf("Index 1 should be e, got %s", im2.GetIdxKey(1))
+	}
+	if im2.GetIdxKey(2) != "" {
+		t.Errorf("Index 2 should be \"\", got %s", im2.GetIdxKey(2))
 	}
 }
